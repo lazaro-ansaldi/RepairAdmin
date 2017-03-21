c1c8106981c2cb7eb62f1612185f646e52505e3c
Lazaro <Lazaro@LAZARO-ASUS>
2017-03-21 00:40:25 -0300

    Creacion proyectos. Referencias. Objetos entidades.

---
+++ ManagePhones/Entities/Modelo.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Entities
+{
+    class Modelo
+    {
+        private int _id;
+        private string _descripcion;
+
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+    }
+}
---
+++ ManagePhones/Util/AppException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    class AppException : Exception
+    {
+        private Exception _ex;
+
+        public Exception Ex
+        {
+            get { return _ex; }
+        }
+        public AppException(string message) : base(message)
+        {
+
+        }
+
+        public AppException()
+        {
+
+        }
+
+        public AppException(string message, string level, Exception ex) : base(message, ex)
+        {
+            _ex = ex;
+            ExType type = (ExType)Enum.Parse(typeof(ExType), level);
+            switch (type)
+            {
+                case ExType.Error:
+                    //Logger.LogError("MESSAGE: " + ex.Message + "\n *-*-*-*-*- \nSTACK TRACE: " + ex.StackTrace);
+                    break;
+                case ExType.Fatal:
+                    //Logger.LogFatal("MESSAGE: " + ex.Message + "\n *-*-*-*-*- \nSTACK TRACE: " + ex.StackTrace);
+                    break;
+            }
+        }
+
+        public enum ExType
+        {
+            Error,
+            Fatal
+        }
+    }
+}
